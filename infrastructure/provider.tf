c22e6d8507d0629f9bb438ff82807eb9f4a9d110
khanhdoan-2809 <khanhdoan21.work@gmail.com>
2024-08-24 00:56:54 +0700

    update terraform backend

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,8 +1,9 @@
 terraform {
     backend "s3" {
-        bucket  = "terraform-backend"
+        bucket  = "terraform-backend-today"
         key     = "pikachu.tfstate"
         region  = "ap-southeast-1"
+        shared_credentials_files = ["\\Users\\khanh\\.aws\\credentials"]
     }
 }
 
